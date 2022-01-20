56c52a0a80a4b55d9396980f59dd596b2e9e044f
FabioCrippa <crippa.fabiocrippa@gmail.com>
2022-01-20 10:05:42 -0300

    primeiro commit

---
+++ exercicios.js
@@ -0,0 +1,79 @@
+=>Arithmetic operators
+--------------------------
+--------------------------
+
+operador modulo(%)
+//console.log(4%2) resto 0
+
+operador adicao(+)
+//console.log(2+2)
+
+operador subtracao(-)
+//console.log(10-5)
+
+operador multiplicacao(*)
+//console.log(10*5)
+
+operador divisao(/)
+//console.log(10/2)
+
+operador incremento(++)
+//let numero = 10;
+//console.log(++numero) imprime 11
+
+operador decremento(++)
+//let numero = 10;
+//console.log(--numero) imprime 9
+
+operador comparacao simples(==)
+//5==7
+//retorna booleano: true ou false
+
+operador comparacao rigorosa(===)
+//retorna booleano: true ou false
+//compara o valor e o tipo de dados
+
+//para retornar true, o valor e o dado deve ser iguais 
+//5==="5" false. Sera false pois temos uma string
+
+operador desigualdade simples(!=)
+//retorna booleano: true ou false
+//pergunta apenas pelo valor. E o valor nao e diferente
+//5!= "5"; O 5 e diferente de "5?"
+
+operador desigualdade rigorosa(!==)
+//retorna booleano: true ou false
+//pergunta o dado e o valor 
+//5!== "5"; valor e igual mas o tipo de dado nao e
+
+
+operador comparacao(><)
+//retorna booleano: true ou false
+//console.log(5>3) retorna true
+
+=>Logical operators
+------------------------
+------------------------
+
+operador and(&)
+//let dia = "domingo"
+//let clima = "ensolarado"
+
+//dia == "domingo" && clima == "ensolarado"
+//retorna true
+//para ser true, todas as frases devem ser verdadeiras
+
+operador or(||)
+//8 >= 15 || 8!= 13
+//false      true
+//para ser true, uma das operacoes deve ser verdadeira
+
+operador concatenacao(+)
+//let nome = "Joana";
+//let sobrenome = "D`Arc";
+//nome + " " + sobrenome
+
+operador negacao(!)
+//retorna booleano: true ou false
+//console.log(!5 >3); 
+//retorna false, pois o operador ! nega a afirmacao verdadeira
